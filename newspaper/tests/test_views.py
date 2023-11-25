7cc0cc31dae46a47eb02ddab16bcf66b8b1dfefe
Roman Zhadan <21gehassig@gmail.com>
2023-11-25 23:36:48 +0200

    add test for views.py

--- newspaper/tests/test_views.py
+++ newspaper/tests/test_views.py
@@ -1,29 +1,39 @@
-# from django.test import TestCase, Client
-# from django.urls import reverse
-#
-# from newspaper.models import Redactor, Topic, Newspaper
-#
-#
-# NEWSPAPER_LIST_URL = reverse("newspaper:newspaper-list")
-#
-#
-# class NewspaperListViewTest(TestCase):
-#     @classmethod
-#     def setUpTestData(cls):
-#         topic = Topic.objects.create(name='Test Topic')
-#         for i in range(10):
-#             Newspaper.objects.create(title=f'Test Newspaper {i}', topic=topic)
-#
-#     def test_newspaper_list_view(self):
-#         response = self.client.get(reverse(NEWSPAPER_LIST_URL))
-#         self.assertEqual(response.status_code, 200)
-#         self.assertTemplateUsed(response, "newspaper/newspaper_list.html")
-#         self.assertTrue('paginator' in response.context)
-#         self.assertTrue('page_obj' in response.context)
-#         self.assertEqual(len(response.context['page_obj']), 5)
-#
-#     def test_newspaper_list_view_search(self):
-#         response = self.client.get(reverse(NEWSPAPER_LIST_URL))
-#         self.assertEqual(response.status_code, 200)
-#         self.assertTrue('page_obj' in response.context)
-#         self.assertEqual(len(response.context['page_obj']), 10)
+from django.test import TestCase
+from django.urls import reverse
+
+from newspaper.forms import NewspaperSearchForm
+from newspaper.models import Newspaper, Redactor, Topic
+
+
+class ViewsTest(TestCase):
+    def setUp(self):
+        self.user = Redactor.objects.create_user(username='testuser', password='testpassword')
+        self.client.login(username='testuser', password='testpassword')
+
+    def test_index_view(self):
+        response = self.client.get(reverse("newspaper:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "newspaper/index.html")
+
+    def test_newspaper_list_view(self):
+        self.client.login(username="testuser", password="12345")
+        response = self.client.get(reverse("newspaper:newspaper-list"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "newspaper/newspaper_list.html")
+
+    def test_search_form_initialization(self):
+        response = self.client.get(reverse("newspaper:newspaper-list"))
+        form = response.context['search_form']
+        self.assertIsInstance(form, NewspaperSearchForm)
+
+    def test_topic_list_view(self):
+        self.client.login(username="testuser", password="12345")
+        response = self.client.get(reverse("newspaper:topic-list"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "newspaper/topic_list.html")
+
+    def test_redactor_list_view(self):
+        self.client.login(username="testuser", password="12345")
+        response = self.client.get(reverse("newspaper:redactor-list"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "newspaper/redactor_list.html")
